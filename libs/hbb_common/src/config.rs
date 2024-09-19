cb2e72ad8d26f36043667ce902d192bb5ca0dcd8
LiShaosong <148532645+lishaosongs@users.noreply.github.com>
2024-09-19 10:58:50 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rdv1.7s3s.cn"];
+pub const PUBLIC_RS_PUB_KEY: &str = "uUnb9eVVNZskn4xjkflCTKbO1CVCax4dg+nw9C26gkQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
